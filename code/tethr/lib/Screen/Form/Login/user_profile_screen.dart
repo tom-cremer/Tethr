80669d8fd6fb25a9ef558841d6f9c25b9307061c
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-09 03:56:53 +0100

    Animation on the Overlay and added qrcode

--- code/tethr/lib/Screen/Form/Login/user_profile_screen.dart
+++ code/tethr/lib/Screen/Form/Login/user_profile_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:tethr/Styles/card_styles.dart';
+import 'package:tethr/Styles/colors.dart';
+import 'package:tethr/Widget/card_stack.dart';
 
-class UserProfileScreen extends StatelessWidget {
+class UserProfileScreen extends StatefulWidget {
   final Map<String, dynamic> userData;
   final List<Map<String, dynamic>>? purchaseData;
   final List<Map<String, dynamic>>? rewardsData;
@@ -15,12 +19,46 @@ class UserProfileScreen extends StatelessWidget {
   });
 
   @override
+  State<UserProfileScreen> createState() => _UserProfileScreenState();
+}
+
+class _UserProfileScreenState extends State<UserProfileScreen>
+    with TickerProviderStateMixin {
+  bool _showOverlay = false;
+  double _dragPosition = 0;
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 500), () {
+      setState(() {
+        _showOverlay = true;
+      });
+    });
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final gradient = GradientStyles.getGradient(
+        widget.userData['activeItems']['iconDecoration']);
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
         actions: [
-          if (isCurrentUser)
+          if (widget.isCurrentUser)
             IconButton(
               icon: const Icon(Icons.edit),
               onPressed: () {
@@ -34,7 +72,7 @@ class UserProfileScreen extends StatelessWidget {
             },
           )
         ],
-        leading: isCurrentUser
+        leading: widget.isCurrentUser
             ? IconButton(
                 icon: const Icon(Icons.star),
                 color: Colors.amber,
@@ -44,108 +82,169 @@ class UserProfileScreen extends StatelessWidget {
               )
             : null,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
+      body: SingleChildScrollView(
+        physics: const NeverScrollableScrollPhysics(),
+        child: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              _buildHeader(),
-              const SizedBox(height: 20),
-              if (isCurrentUser) _buildEditAndRewardsButtons(context),
-              const SizedBox(height: 20),
-              _buildSectionTitle('Purchases'),
-              _buildPurchasesList(),
-              const SizedBox(height: 20),
-              _buildSectionTitle('Rewards'),
-              _buildRewardsList(),
+              Center(
+                child: CardStack(
+                  userData: widget.userData,
+                ),
+              ),
+              const SizedBox(height: 25),
+              GestureDetector(
+                onVerticalDragUpdate: (details) {
+                  setState(() {
+                    // Track vertical drag and update the position
+                    _dragPosition += details.primaryDelta!;
+                  });
+                },
+                onVerticalDragEnd: (details) {
+                  // If the drag exceeds a certain threshold, go back to the previous screen
+                  if (_dragPosition > 200) {
+                    Navigator.pop(context); // Go back to the previous screen
+                  } else {
+                    // Otherwise, animate back to the original position
+                    _animationController.forward(
+                        from: 0); // Reset animation to 0
+                    setState(() {
+                      _dragPosition = 0; // Reset the drag position
+                    });
+                  }
+                },
+                child: AnimatedBuilder(
+                  animation: _animationController,
+                  builder: (context, child) {
+                    return AnimatedContainer(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.fastLinearToSlowEaseIn,
+                      margin: EdgeInsets.only(
+                        top: _showOverlay
+                            ? (_dragPosition < 0 ? 0 : _dragPosition)
+                            : MediaQuery.of(context).size.height,
+                      ),
+                      // Use the full screen height for the overlay
+                      height: MediaQuery.of(context).size.height -
+                          AppBar().preferredSize.height -
+                          kLargeCardHeight -
+                          25,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[200],
+                        borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(30),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Container(
+                              width: 40,
+                              height: 4,
+                              decoration: BoxDecoration(
+                                gradient: gradient,
+                                borderRadius: BorderRadius.circular(2),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Container(
+                                  height: 230,
+                                  width: 230,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: QrImageView(
+                                      data:
+                                          'https://tomcremer.be/@${widget.userData['username']}',
+                                      eyeStyle: QrEyeStyle(
+                                        eyeShape: QrEyeShape.square,
+                                        color: gradient.colors[1],
+                                      ),
+                                      dataModuleStyle: QrDataModuleStyle(
+                                        dataModuleShape:
+                                            QrDataModuleShape.square,
+                                        color: gradient.colors[1],
+                                      ),
+                                      version: QrVersions.auto,
+                                      size: 200),
+                                ),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Container(
+                                      height: 50,
+                                      width: 70,
+                                      decoration: BoxDecoration(
+                                        color: kBlue,
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                      child: Center(
+                                        child: IconButton(
+                                          onPressed: () {
+
+                                          },
+                                          icon: const Icon(Icons.send_outlined),
+                                          color: Colors.grey[800],
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 30),
+                                    Container(
+                                      height: 50,
+                                      width: 70,
+                                      decoration: BoxDecoration(
+                                        color: kGreen,
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                      child: Center(
+                                        child: IconButton(
+                                          onPressed: () {
+                                            // Download the QR code
+                                          },
+                                          icon: const Icon(
+                                              Icons.download_outlined),
+                                          color: Colors.grey[800],
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 30),
+                                    Container(
+                                      height: 50,
+                                      width: 70,
+                                      decoration: BoxDecoration(
+                                        color: kPurple,
+                                        borderRadius: BorderRadius.circular(50),
+                                      ),
+                                      child: Center(
+                                        child: IconButton(
+                                          onPressed: () {
+                                            // Action for the NFC button
+                                          },
+                                          icon: const Icon(Icons.nfc_outlined),
+                                          color: Colors.grey[800],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
       ),
-    );
-  }
-
-  Widget _buildHeader() {
-    return Row(
-      children: [
-        CircleAvatar(
-          radius: 40,
-          child: userData['profilePicture'] == null
-              ? const Icon(Icons.person, size: 40)
-              : null,
-        ),
-        const SizedBox(width: 16),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              userData['name'] ?? userData['username'] ?? 'Anonymous',
-              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            Text(userData['email'] ?? 'No email available'),
-          ],
-        ),
-      ],
-    );
-  }
-
-  Widget _buildEditAndRewardsButtons(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            // Navigate to Edit Profile screen.
-          },
-          child: const Text('Edit Profile'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            // Navigate to Rewards screen.
-          },
-          child: const Text('View Rewards'),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildSectionTitle(String title) {
-    return Text(
-      title,
-      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-    );
-  }
-
-  Widget _buildPurchasesList() {
-    if (purchaseData == null || purchaseData!.isEmpty) {
-      return const Text('No purchases yet.');
-    }
-
-    return Column(
-      children: purchaseData!
-          .map((purchase) => ListTile(
-                leading: const Icon(Icons.shopping_cart),
-                title: Text(purchase['name'] ?? 'Unknown Item'),
-                subtitle: Text(purchase['type'] ?? 'Unknown Type'),
-              ))
-          .toList(),
-    );
-  }
-
-  Widget _buildRewardsList() {
-    if (rewardsData == null || rewardsData!.isEmpty) {
-      return const Text('No rewards earned yet.');
-    }
-
-    return Column(
-      children: rewardsData!
-          .map((reward) => ListTile(
-                leading: const Icon(Icons.star, color: Colors.amber),
-                title: Text(reward['name'] ?? 'Unknown Reward'),
-                subtitle: Text(reward['description'] ?? 'No description'),
-              ))
-          .toList(),
     );
   }
 }
