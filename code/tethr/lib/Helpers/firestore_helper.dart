8d4d1f3f011ef433771815f998aba3733a4ceb61
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-11 04:25:59 +0100

    Refactored Firestore Helper to use Dto package

--- code/tethr/lib/Helpers/firestore_helper.dart
+++ code/tethr/lib/Helpers/firestore_helper.dart
@@ -1,196 +1,189 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:dto/models.dart' as dto;
 
 class FirestoreHelper {
   static final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  /// Get logged-in user's data.
-  static Future<Map<String, dynamic>?> getUserData() async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
+  /// References
+  static final CollectionReference<dto.User> usersRef =
+  _firestore.collection('users').withConverter<dto.User>(
+    fromFirestore: (snapshot, _) => dto.User.fromJson(snapshot.data()!),
+    toFirestore: (user, _) => user.toJson(),
+  );
+
+  static final CollectionReference<dto.ShopItem> shopRef =
+  _firestore.collection('shop').withConverter<dto.ShopItem>(
+    fromFirestore: (snapshot, _) =>
+        dto.ShopItem.fromJson(snapshot.data()!),
+    toFirestore: (item, _) => item.toJson(),
+  );
+
+  static final CollectionReference<dto.Reward> rewardsRef =
+  _firestore.collection('rewards').withConverter<dto.Reward>(
+    fromFirestore: (snapshot, _) =>
+        dto.Reward.fromJson(snapshot.data()!),
+    toFirestore: (reward, _) => reward.toJson(),
+  );
+
+  /// Get logged-in user's data as a `User` DTO.
+  static Future<dto.User?> getUserData() async {
+    final auth.User? authUser = auth.FirebaseAuth.instance.currentUser;
+    if (authUser == null) {
       throw Exception('No user is currently logged in.');
     }
 
-    final DocumentSnapshot userDoc =
-    await _firestore.collection('users').doc(user.uid).get();
-
-    if (userDoc.exists) {
-      return userDoc.data() as Map<String, dynamic>;
-    }
-    return null;
+    final userDoc = await usersRef.doc(authUser.uid).get();
+    return userDoc.data();
   }
 
-  /// Get user data by UID.
-  static Future<Map<String, dynamic>?> getUserDataByUid(String uid) async {
-    final DocumentSnapshot userDoc =
-    await _firestore.collection('users').doc(uid).get();
-
-    if (userDoc.exists) {
-      return userDoc.data() as Map<String, dynamic>;
-    }
-    return null;
+  /// Create a new user in Firestore.
+  static Future<void> createUser(String uid, dto.User newUser) async {
+    await usersRef.doc(uid).set(newUser);
   }
 
-  /// Get logged-in user's followers.
-  static Future<List<Map<String, dynamic>>> getUserFollowers() async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
+  /// Get user data by UID as a `User` DTO.
+  static Future<dto.User?> getUserDataByUid(String uid) async {
+    final userDoc = await usersRef.doc(uid).get();
+    return userDoc.data();
+  }
+
+  /// Get logged-in user's followers as a list of `Follow` DTOs.
+  static Future<List<dto.Follow>> getUserFollowers() async {
+    final auth.User? authUser = auth.FirebaseAuth.instance.currentUser;
+    if (authUser == null) {
       throw Exception('No user is currently logged in.');
     }
 
-    final QuerySnapshot followersSnapshot = await _firestore
-        .collection('users')
-        .doc(user.uid)
-        .collection('followers')
-        .get();
+    final followersRef = usersRef.doc(authUser.uid).collection('followers').withConverter<dto.Follow>(
+      fromFirestore: (snapshot, _) => dto.Follow.fromJson(snapshot.data()!),
+      toFirestore: (follow, _) => follow.toJson(),
+    );
 
-    return followersSnapshot.docs
-        .map((doc) => doc.data() as Map<String, dynamic>)
-        .toList();
+    final followersSnapshot = await followersRef.get();
+    return followersSnapshot.docs.map((doc) => doc.data()).toList();
   }
 
-  /// Get logged-in user's purchase history.
-  static Future<List<Map<String, dynamic>>> getUserPurchases() async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
+  /// Get logged-in user's purchase history as a list of `UserPurchase` DTOs.
+  static Future<List<dto.UserPurchase>> getUserPurchases() async {
+    final auth.User? authUser = auth.FirebaseAuth.instance.currentUser;
+    if (authUser == null) {
       throw Exception('No user is currently logged in.');
     }
 
-    final QuerySnapshot purchasesSnapshot = await _firestore
-        .collection('users')
-        .doc(user.uid)
-        .collection('purchases')
-        .get();
+    final purchasesRef = usersRef.doc(authUser.uid).collection('purchases').withConverter<dto.UserPurchase>(
+      fromFirestore: (snapshot, _) =>
+          dto.UserPurchase.fromJson(snapshot.data()!),
+      toFirestore: (purchase, _) => purchase.toJson(),
+    );
 
-    return purchasesSnapshot.docs
-        .map((doc) => doc.data() as Map<String, dynamic>)
-        .toList();
+    final purchasesSnapshot = await purchasesRef.get();
+    return purchasesSnapshot.docs.map((doc) => doc.data()).toList();
   }
 
-  /// Get logged-in user's rewards history.
-  static Future<List<Map<String, dynamic>>> getUserRewards() async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
+  /// Get logged-in user's rewards history as a list of `UserReward` DTOs.
+  static Future<List<dto.UserReward>> getUserRewards() async {
+    final auth.User? authUser = auth.FirebaseAuth.instance.currentUser;
+    if (authUser == null) {
       throw Exception('No user is currently logged in.');
     }
 
-    final QuerySnapshot rewardsSnapshot = await _firestore
-        .collection('users')
-        .doc(user.uid)
-        .collection('rewards')
-        .get();
+    final rewardsRef = usersRef.doc(authUser.uid).collection('rewards').withConverter<dto.UserReward>(
+      fromFirestore: (snapshot, _) =>
+          dto.UserReward.fromJson(snapshot.data()!),
+      toFirestore: (reward, _) => reward.toJson(),
+    );
 
-    return rewardsSnapshot.docs
-        .map((doc) => doc.data() as Map<String, dynamic>)
-        .toList();
+    final rewardsSnapshot = await rewardsRef.get();
+    return rewardsSnapshot.docs.map((doc) => doc.data()).toList();
   }
 
   /// Update logged-in user's data.
-  static Future<void> updateUserData(Map<String, dynamic> updatedData) async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
+  static Future<void> updateUserData(dto.User updatedUser) async {
+    final auth.User? authUser = auth.FirebaseAuth.instance.currentUser;
+    if (authUser == null) {
       throw Exception('No user is currently logged in.');
     }
 
-    final DocumentReference userDocRef =
-    _firestore.collection('users').doc(user.uid);
-
-    await userDocRef.update(updatedData);
+    await usersRef.doc(authUser.uid).set(updatedUser);
   }
 
-  /// Update a user's data in all followers' sub-collections.
-  static Future<void> updateUserInFollowers(String userUid) async {
-    try {
-      // Step 1: Fetch the user's latest data using the helper method.
-      final DocumentSnapshot userDoc =
-      await _firestore.collection('users').doc(userUid).get();
+  /// Fetch all shop items as a list of `ShopItem` DTOs.
+  static Future<List<dto.ShopItem>> getShopItems() async {
+    final shopSnapshot = await shopRef.get();
+    return shopSnapshot.docs.map((doc) => doc.data()).toList();
+  }
 
-      if (!userDoc.exists) {
-        throw Exception("User with UID $userUid does not exist in Firestore.");
-      }
+  /// Fetch all rewards as a list of `Reward` DTOs.
+  static Future<List<dto.Reward>> getAllRewards() async {
+    final rewardsSnapshot = await rewardsRef.get();
+    return rewardsSnapshot.docs.map((doc) => doc.data()).toList();
+  }
 
-      final Map<String, dynamic> userData = userDoc.data() as Map<String, dynamic>;
-
-      // Step 2: Get all users to search for the followers sub-collections.
-      final QuerySnapshot usersSnapshot = await _firestore.collection('users').get();
-
-      for (var doc in usersSnapshot.docs) {
-        final CollectionReference followersCollection =
-        doc.reference.collection('followers');
-
-        final DocumentReference followerDoc = followersCollection.doc(userUid);
-
-        // Step 3: Check if this user exists in the current followers sub-collection.
-        final DocumentSnapshot followerDocSnapshot = await followerDoc.get();
-
-        if (followerDocSnapshot.exists) {
-          // Step 4: Update the user's data in the followers sub-collection.
-          await followerDoc.update(userData);
-        }
-      }
-    } catch (e) {
-      throw Exception("Error updating user in followers: $e");
+  /// Remove the purchased items from the shop items list. (Logged-in user)
+  static Future<List<dto.ShopItem>> removePurchasedItems() async{
+    final shopItems = await getShopItems();
+    final purchaseData = await getUserPurchases();
+    for (final purchase in purchaseData) {
+      shopItems.removeWhere((item) => item.id == purchase.itemId);
     }
-  }
-
-  /// Fetch all shop items.
-  static Future<List<Map<String, dynamic>>> getShopItems() async {
-    final QuerySnapshot shopSnapshot = await _firestore.collection('shop').get();
-    return shopSnapshot.docs
-        .map((doc) => doc.data() as Map<String, dynamic>)
-        .toList();
-  }
-
-  /// Fetch all rewards.
-  static Future<List<Map<String, dynamic>>> getAllRewards() async {
-    final QuerySnapshot rewardsSnapshot =
-    await _firestore.collection('rewards').get();
-    return rewardsSnapshot.docs
-        .map((doc) => doc.data() as Map<String, dynamic>)
-        .toList();
+    return shopItems;
   }
 
   /// Purchase an item for the logged-in user.
   static Future<void> purchaseItem(String itemId) async {
-    final User? user = FirebaseAuth.instance.currentUser;
-    if (user == null) {
+    final auth.User? authUser = auth.FirebaseAuth.instance.currentUser;
+    if (authUser == null) {
       throw Exception('No user is currently logged in.');
     }
 
-    final DocumentSnapshot itemSnapshot =
-    await _firestore.collection('shop').doc(itemId).get();
-
+    final itemSnapshot = await shopRef.doc(itemId).get();
     if (!itemSnapshot.exists) {
       throw Exception("Item with ID $itemId does not exist in the shop.");
     }
 
-    final itemData = itemSnapshot.data() as Map<String, dynamic>;
-
-    final DocumentReference userDocRef =
-    _firestore.collection('users').doc(user.uid);
-    final DocumentReference purchaseRef =
-    userDocRef.collection('purchases').doc(itemId);
+    final dto.ShopItem item = itemSnapshot.data()!;
+    final userDocRef = usersRef.doc(authUser.uid);
 
     await _firestore.runTransaction((transaction) async {
       final userDoc = await transaction.get(userDocRef);
-
       if (!userDoc.exists) {
         throw Exception("User document does not exist.");
       }
 
-      final currentPoints = (userDoc.data() as Map<String, dynamic>)['starPoints'] ?? 0;
-
-      if (currentPoints < itemData['cost']) {
+      final dto.User user = userDoc.data()!;
+      if (user.starPoints < item.cost) {
         throw Exception("Insufficient star points.");
       }
 
-      // Deduct star points and save the purchase.
-      transaction.update(userDocRef, {'starPoints': currentPoints - itemData['cost']});
-      transaction.set(purchaseRef, {
-        'id': itemId,
-        ...itemData,
-        'purchasedAt': FieldValue.serverTimestamp(),
-      });
+      final updatedUser = dto.User(
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        username: user.username,
+        starPoints: user.starPoints - item.cost,
+        links: user.links,
+        activeItems: user.activeItems,
+      );
+
+      transaction.set(userDocRef, updatedUser);
+
+      final purchaseRef = userDocRef.collection('purchases').doc(item.id).withConverter<dto.UserPurchase>(
+        fromFirestore: (snapshot, _) =>
+            dto.UserPurchase.fromJson(snapshot.data()!),
+        toFirestore: (purchase, _) => purchase.toJson(),
+      );
+
+      transaction.set(
+        purchaseRef,
+        dto.UserPurchase(
+          itemId: item.id,
+          name: item.name,
+          type: item.type,
+          icon: item.icon,
+          purchasedAt: Timestamp.now(),
+        ),
+      );
     });
   }
 }
