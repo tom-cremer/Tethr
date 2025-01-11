530cfa17612cdeff451e3d4e797dcd20e5fdc30a
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-11 04:28:53 +0100

    Added SystemUi theme & Preferred Orientation

--- code/tethr/lib/main.dart
+++ code/tethr/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:tethr/Screen/Form/Login/home_screen.dart';
 import 'package:tethr/firebase_options.dart';
 import 'Screen/welcome.dart';
@@ -12,7 +13,6 @@ Future<void> main() async {
   );
   await FirebaseAuth.instance.setLanguageCode("fr");
   runApp(const MyApp());
-
 }
 
 class MyApp extends StatelessWidget {
@@ -20,15 +20,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.transparent,
+        systemNavigationBarIconBrightness: Brightness.dark,
+        systemNavigationBarDividerColor: Colors.transparent,
+      ),
+    );
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       title: 'Tethr',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
         useMaterial3: true,
       ),
-      home: FirebaseAuth.instance.currentUser == null ?
-       Welcome():
-          HomeScreen(),
+      home:
+          FirebaseAuth.instance.currentUser == null ? Welcome() : HomeScreen(),
     );
   }
 }
