f45fa878eb15506b55420bd9cc020308d41aaab1
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-12 12:52:28 +0100

    New Color kGrayLight

--- code/tethr/lib/Styles/colors.dart
+++ code/tethr/lib/Styles/colors.dart
@@ -5,6 +5,7 @@ const kBlue = Color.fromRGBO(169, 228, 233, 1);
 const kPurple = Color.fromRGBO(179, 172, 233, 1);
 const kGreen = Color.fromRGBO(194, 235, 187, 1);
 const kGray = Color.fromRGBO(151, 151, 151, 1);
+const kGrayLight = Color.fromRGBO(177, 177, 177, 1.0);
 const kBlackText = Color.fromRGBO(62, 62, 62, 1);
 const kBlack = Color.fromRGBO(26, 26, 26, 1);
 const kWhite = Color.fromRGBO(255, 255, 255, 1);
