8e86fc68302510f158e19e78c963bd69ad8eabca
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-12 12:57:59 +0100

    Using GoRouter for the profiles routing

--- code/tethr/lib/main.dart
+++ code/tethr/lib/main.dart
@@ -2,18 +2,46 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:go_router/go_router.dart';
 import 'package:tethr/Screen/Form/Login/home_screen.dart';
+import 'package:tethr/Screen/Form/Login/user_profile_screen.dart';
+import 'package:tethr/Screen/settings.dart';
+import 'package:tethr/Screen/welcome.dart';
 import 'package:tethr/firebase_options.dart';
-import 'Screen/welcome.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  await FirebaseAuth.instance.setLanguageCode("fr");
+  await FirebaseAuth.instance.setLanguageCode("en");
   runApp(const MyApp());
 }
+
+final GoRouter _router = GoRouter(
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        final user = FirebaseAuth.instance.currentUser;
+        if (user != null) {
+          return HomeScreen(); // Navigate to HomeScreen if user is logged in
+        } else {
+          return Welcome(); // Navigate to Welcome screen if user is not logged in
+        }
+      },
+      routes: [
+        GoRoute(
+          path: 'profile/:uid',
+          builder: (context, state) {
+            final uid = state.pathParameters['uid']!;
+            return UserProfileScreen(uid: uid);
+          },
+        ),
+      ],
+    ),
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -33,14 +61,13 @@ class MyApp extends StatelessWidget {
       DeviceOrientation.portraitUp,
       DeviceOrientation.portraitDown,
     ]);
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Tethr',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrangeAccent),
         useMaterial3: true,
       ),
-      home:
-          FirebaseAuth.instance.currentUser == null ? Welcome() : HomeScreen(),
+      routerConfig: _router,
     );
   }
 }
