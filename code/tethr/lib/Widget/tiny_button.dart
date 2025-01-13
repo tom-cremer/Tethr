345e1c291e980ba8042dab606cdace9e6baab3b6
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-14 00:26:22 +0100

    Added Animation using Ink, Inkwell

--- code/tethr/lib/Widget/tiny_button.dart
+++ code/tethr/lib/Widget/tiny_button.dart
@@ -22,30 +22,44 @@ class TinyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: containerColor,
-          borderRadius: BorderRadius.circular(50),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-          child: Center(
-            child: Column(
-              children: [
-                Icon(icon, color: iconColor),
-                Text(label,
-                    style: TextStyle(
+    return Material(
+        color: Colors.transparent, // Set to transparent to avoid overriding the Ink color
+        child: InkWell(
+          onTap: onTap,
+          highlightColor: Colors.grey.withOpacity(0.3),
+          splashColor: Colors.black.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(50.0),
+          child: Ink(
+            width: 90.0,
+            height: 60.0,
+            decoration: BoxDecoration(
+              color: containerColor, // Your background color
+              borderRadius: BorderRadius.circular(50.0),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              child: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min, // Ensures the button adjusts to its content
+                  children: [
+                    Icon(icon, color: iconColor),
+                    Text(
+                      label,
+                      style: TextStyle(
                         color: textColor,
                         fontWeight: FontWeight.w600,
                         fontSize: 11,
-                        fontFamily: 'Lexend')),
-              ],
+                        fontFamily: 'Lexend',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+
+
   }
 }
