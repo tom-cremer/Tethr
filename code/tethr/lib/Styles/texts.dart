fbd836f133cb428b3cfb91d7ed2b435d8d6675c0
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-14 00:27:06 +0100

    Text for SnackBar

---
+++ code/tethr/lib/Styles/texts.dart
@@ -0,0 +1,12 @@
+
+String kErrorFetchUser = 'Oops! Couldn\'t fetch user data 😶';
+String kInvalidQrCode = 'Oops! This Qr Code is not valid 🤨';
+String kNiceTry = 'Nice Try! But you cannot scan own QR code 😆';
+String kUserDoesntExist = 'Oops! Unfortunatly this user does not exist 😔';
+String kInsufficientStarPoints = 'Oops! Insufficient star points 😅';
+String kLoginSuccessful = 'Success! You are now logged in 😎';
+String kAlreadyFollowing = 'You are already following this user 😅';
+String kFollowed = 'You are now following this user 😎';
+String kUnfollowed = 'You have unfollowed this user 😔';
+String kErrorUnfollow = 'Oops! Couldn\'t unfollow this user 😶';
+String kProfileUpdatedSuccess = 'Success! Profile updated successfully 😎';
