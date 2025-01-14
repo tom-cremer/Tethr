50df7fe870a8b34a2a9f3fa3c4df13bc09334daa
Cremer Tom <tom.cremer@student.hepl.be>
2025-01-14 22:38:58 +0100

    Removed code causing an impossible register

--- code/tethr/lib/Screen/Form/Register/register_screen.dart
+++ code/tethr/lib/Screen/Form/Register/register_screen.dart
@@ -194,19 +194,6 @@ class _RegisterScreenState extends State<RegisterScreen> {
                               if (value.length < 8) {
                                 return 'Password must be at least 8 characters';
                               }
-                              if (value.contains(RegExp('A-Z'))) {
-                                return 'Password must contain at least one uppercase letter';
-                              }
-                              if (value.contains(RegExp('a-z'))) {
-                                return 'Password must contain at least one lowercase letter';
-                              }
-                              if (value.contains(RegExp(r'[0-9]'))) {
-                                return 'Password must contain at least one number';
-                              }
-                              if (value.contains(
-                                  RegExp(r'[!@#$%^&*(),.?":{}|<>]'))) {
-                                return 'Password must contain at least one special character';
-                              }
                               return null;
                             },
                           ),
